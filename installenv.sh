45dffbae7e15a6d6da22bad4b37dc65d3484e956
Faisal Shah <faisal.shah@halliburton.com>
2018-11-17 22:07:24 -0600

    Add bashrc related shortcuts, and stuff to installenv

--- installenv.sh
+++ installenv.sh
@@ -3,6 +3,7 @@
 echo ***********************
 echo Installing apt packages
 sudo apt install automake autoconf build-essential pkg-config libevent-dev libncurses5-dev links git gdebi tree curl silversearcher-ag tmux python3-pip ranger
+sudo apt install cowsay fortune
 
 echo ********************
 echo Installing Powerline
@@ -58,6 +59,11 @@ vim -c "PlugInstall" -c "qa"
 echo ***************
 echo Installing Tmux
 cp tmux.conf ~/.tmux.conf
+
+echo **************************
+echo Installing .localshortcuts
+mkdir -p ~/.local/bin
+cp ./local/* ~/.local/bin
 
 echo *********************
 echo Installing PathPicker
