b0b8b9140b6b654bb2467feef569e28c463abdc7
Faisal Shah <faisal.shah@gmail.com>
2019-07-29 12:15:30 -0500

    Install powerline fonts using package manager
    Removed the manual installation of powerline fonts, instead use package
    manager to install the fonts-powerline package. Also, removed the option
    to install powerline system wide. Now, it only installs it locally.
    Sytem wide installation was problematic anyways ...

--- installenv.sh
+++ installenv.sh
@@ -7,7 +7,7 @@ echo ***********************
 echo Installing apt packages
 sudo apt install -y automake autoconf build-essential pkg-config libevent-dev
 sudo apt install -y libncurses5-dev links git gitk gdebi tree curl silversearcher-ag python3-pip
-sudo apt install -y cowsay fortune xsel
+sudo apt install -y cowsay fortune xsel fonts-powerline
 
 echo ***************************
 echo Installing fd (file finder)
@@ -54,29 +54,13 @@ sudo gdebi ../fpp_0.7.2_noarch.deb
 echo ********************
 echo Installing Powerline
 # https://askubuntu.com/questions/283908/how-can-i-install-and-use-powerline-plugin
-wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
-wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-echo "Install Powerline local or system-wide? (system-wide has problems)"
-select opt in "local" "system"; do
-  case $opt in
-    local )
-      mkdir -p ~/.local/share/fonts/ && mv PowerlineSymbols.otf ~/.local/share/fonts/
-      fc-cache -vf ~/.local/share/fonts
-      mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
-      pip3 install --user psutil
-      pip3 install --user powerline-status
-      break
-      ;;
-    system )
-      sudo mv PowerlineSymbols.otf /usr/share/fonts/
-      sudo fc-cache -vf
-      sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
-      sudo pip3 install psutil
-      sudo pip3 install powerline-status
-      break
-      ;;
-  esac
-done
+# wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+# wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+# mkdir -p ~/.local/share/fonts/ && mv PowerlineSymbols.otf ~/.local/share/fonts/
+# fc-cache -vf ~/.local/share/fonts
+# mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+pip3 install --user psutil
+pip3 install --user powerline-status
 
 PWRLN_REPO_ROOT="$(pip3 show powerline-status | awk '{if ($1=="Location:") print $2}')"
 PWRLN_PATH="$PWRLN_REPO_ROOT"/powerline
@@ -104,4 +88,3 @@ echo *******************************
 echo Installing FZF and Vim Pluggins
 cp vimrc ~/.vimrc
 vim -c "PlugInstall" -c "qa"
-
