c841eb1fd27d97ee480da92f7279e18b1dd72cb1
=Faisal Shah <=faisal.shah@gmail.com>
2021-04-07 16:30:21 -0500

    Move files around, add bash functions and Konsole colorschem

---
+++ local/bin/my-utils.sh
@@ -0,0 +1,18 @@
+# Gets the download url for the latest release of a package provided via GitHub Releases
+# Usage: ghrelease USER REPO [PATTERN]
+ghrelease() {
+	curl -s "https://api.github.com/repos/$1/$2/releases/latest" | grep -o "http.*${3:-deb}"
+}
+
+# Installs a local or remote(http/https) deb package and removes it after installation
+installdeb() {
+	set -e
+	loc="/tmp/install.deb"
+	case $1 in
+	http*) sudo wget -O "$loc" $1;;
+	*) loc="$1"
+	esac
+	sudo dpkg -i "$loc"
+	sudo apt -f install
+	sudo rm -f "$loc"
+}
