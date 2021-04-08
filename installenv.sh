55da7429a730da1c4aa5e1e16f7941ed4f71c891
Faisal Shah <faisal.shah@gmail.com>
2021-04-08 00:53:53 -0500

    bat-extras tests now passing with ripgrep 12.0+

--- installenv.sh
+++ installenv.sh
@@ -44,7 +44,7 @@ echo "*********************"
 echo "Installing bat-extras"
 git clone https://github.com/eth-p/bat-extras.git
 cd bat-extras
-./build.sh --install --prefix=~/.local --no-verify
+./build.sh --install --prefix=~/.local
 cd ..
 
 echo "****************"
